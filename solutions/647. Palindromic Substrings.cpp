cde3bfcbe1a5e21b78fd0a4610856c6a32533ce5
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2020-10-11 11:10:42 +0530

    4 ms

--- solutions/647. Palindromic Substrings.cpp
+++ solutions/647. Palindromic Substrings.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        long long dp[1003][1004];
-​
-        int n=s.length();
-        memset(dp,0,sizeof(dp));
-        for(int i=1;i<=n;i++)
-            dp[i][i]=1;
-        for(int i=n;i>=1;i--){
-            for(int j=n;j>=i+1;j--){
-                if(s[i-1]==s[j-1])
-                {
-                    if(i+1>=j-1)
-                        dp[i][j]+=1;
-                    else
-                         dp[i][j]+=dp[i+1][j-1];
-                    
-                }
-            }
-        }
-        long long ans=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                ans+=dp[i][j];
-                // cout<<dp[i][j]<<" ";
-            }
-            // cout<<endl;
-                
-        }
+        int ans=0,n=s.length();
+       for(int i=0;i<n;i++){
+           ans++;
+           int j=i-1,k=i+1;
+           while(k<n&&j>=0){
+               if(s[k]==s[j])
+                   ans++;
+               else
+                   break;
+               k++;
+               j--;
+           }
+           j=i,k=i+1;
+           while(k<n&&j>=0){
+               if(s[k]==s[j])
+                   ans++;
+               else
+                   break;
+               k++;
+               j--;
+           }
+           // j=i-1,k=i;
+           // while(k<n&&j>=0){
+           //     if(s[k]==s[j])
+           //         ans++;
+           //     else
+           //         break;
+           //     k++;
+           //     j--;
+           // }
+       }
         return ans;
-        // return 0;
     }
 };
