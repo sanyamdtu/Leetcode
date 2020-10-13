935899cb3cf823d294c668b27dc0415a95e66d4c
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2020-10-13 13:41:46 +0530

    0 ms

--- solutions/343. Integer Break.cpp
+++ solutions/343. Integer Break.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int dp[100][100];
-    int func(int n,int c=0){
-        if(n==0)
-            return (c>=2);
-        if(dp[n][c]!=-1)
-            return dp[n][c];
+    int dp[100];
+    int func(int n){
+        if(n<=1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
         int ans=0;
         for(int i=1;i<=n;i++){
-            ans=max(i*func(n-i,c+1),ans);
+            ans=max(i*func(n-i),ans);
         }
-        return dp[n][c] =ans;
+        return dp[n] =ans;
     }
     int integerBreak(int n) {
         // if(n==2)
         //     return 1;
         memset(dp,-1,sizeof(dp));
-        return func(n);
+        int ans=0;
+        for(int i=1;i<n;i++)
+            ans=max(i*func(n-i),ans);
+        return ans;
     }
 };
