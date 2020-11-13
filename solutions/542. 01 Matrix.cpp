301be4a63c4a0790d40547c9f7a5c1baf53ccdd3
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2020-11-13 11:59:14 +0530

    132 ms

--- solutions/542. 01 Matrix.cpp
+++ solutions/542. 01 Matrix.cpp
@@ -1,23 +1,3 @@
-class Solution {
-public:
-    int n;
-    int m;
-    vector<vector<int>> updateMatrix(vector<vector<int>>& arr) {
-        n=arr.size();
-        m=arr[0].size();
-        // cout<<m<<" "<<n;
-        vector<vector<int>>v(n,vector<int>(m));
-        int dist[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++)
-                dist[i][j]=INT_MAX;
-        }
-        queue<pair<int,int>>q ;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==0){
-                    q.push({i,j});
-                    dist[i][j]=0;
                 }
                     
             }
@@ -30,11 +10,11 @@ public:
             for(int i=0;i<4;i++){
                 int x=a.first+dx[i];
                 int y=a.second+dy[i];
-                if(x>=0&&y>=0&&x<n&&y<m)
-                if(dist[a.first][a.second]+1<dist[x][y]){
+                if(x>=0&&y>=0&&x<n&&y<m&&dist[x][y]==INT_MAX){
+                // if(dist[a.first][a.second]+1<dist[x][y]){
                     dist[x][y]=dist[a.first][a.second]+1;
                     q.push({x,y});
-                }
+                }
             }
         }
         for(int i=0;i<n;i++){
