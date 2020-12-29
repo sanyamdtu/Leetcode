bafa7a62ddf58ec444029c9d31e5731d4c1c5713
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2020-12-29 19:11:45 +0530

    100 ms

--- solutions/1162. As Far from Land as Possible.cpp
+++ solutions/1162. As Far from Land as Possible.cpp
@@ -2,44 +2,38 @@ class Solution {
 public:
     int maxDistance(vector<vector<int>>& arr) {
         int n=arr.size();
-        queue<pair<int,int>> q;
+        if(n==0)
+            return 0;
         int dist[n][n];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                dist[i][j]=INT_MAX;
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++)
+            {
                 if(arr[i][j]==1){
-                    q.push({i,j});
                     dist[i][j]=0;
+                    q.push({i,j});
                 }
-                    
             }
         }
         int dx[]={1,0,0,-1};
-        int dy[]={0,1,-1,0};
-        
+        int dy[]={0,-1,1,0};
+        int ans=-1;
         while(!q.empty()){
             auto a=q.front();
             q.pop();
             int x=a.first;
             int y=a.second;
             for(int i=0;i<4;i++){
-                int p= x+dx[i];
-                int qw= y+dy[i];
-                if(p>=0&&qw>=0&&p<n&&qw<n&&arr[p][qw]==0&&dist[p][qw]==INT_MAX){
-                    dist[p][qw]=dist[x][y]+1;
-                    q.push({p,qw});
+                int p=x+dx[i];
+                int qq=y+dy[i];
+                if(p>=0&&qq>=0&&p<n&&qq<n&&arr[p][qq]==0){
+                    arr[p][qq]=1;
+                    dist[p][qq]=1+dist[x][y];
+                    ans=max(dist[p][qq],ans);
+                    q.push({p,qq});
                 }
             }
         }
-        int a=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
-            a=max(a,dist[i][j]);
-        }
-        if(a==0||a==INT_MAX)
-            return -1;
-        return a;
+        return ans;
     }
 };
