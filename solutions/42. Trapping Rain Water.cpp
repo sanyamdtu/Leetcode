3413a5a08fec9e08ea766f8b37e58028262f6bab
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-01-14 15:54:08 +0530

    24 ms

---
+++ solutions/42. Trapping Rain Water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+        int n=arr.size();
+        if(n<=2)
+            return 0;
+        int l=0,r=n-1,a=0,b=0,ans=0;
+        while(l<=r){
+            cout<<arr[l]<<" "<<arr[r]<<" "<<a<<" "<<b<<" "<<ans<<endl;
+            if(arr[l]<=arr[r]){
+                if(arr[l]>=a)
+                    a=arr[l];
+                else
+                    ans+=a-arr[l];
+                l++;
+            }
+            else{
+                if(arr[r]>=b)
+                    b=arr[r];
+                else 
+                    ans+=b-arr[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
