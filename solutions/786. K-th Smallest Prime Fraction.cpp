e2bb27affb5b17a1f8796a75d1b386098efed584
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-02-11 20:04:31 +0530

    4 ms

--- solutions/786. K-th Smallest Prime Fraction.cpp
+++ solutions/786. K-th Smallest Prime Fraction.cpp
@@ -1,34 +1,36 @@
-vector<int> e;
-struct node {
-    int i,j;
-};
-struct comp{
-    bool operator()(node a,node b){
-        int p=e[a.i]*e[b.j];
-        int q=e[b.i]*e[a.j];
-        return p>q;
-    }
-};
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        int n=arr.size();
-        e=arr;
-        vector<int> ans;
-        if(n==0)
-            return ans;
-        priority_queue<node,vector<node>,comp> q;
-        for(int i=0;i<n-1;i++)
-            q.push({i,n-1});
-        int c=1;
-        while(c<k){
-            c++;
-            auto a=q.top();
-            q.pop();
-            if(a.i!=a.j-1)
-                q.push({a.i,a.j-1});
+        double lo=0.0,hi=1.0,mid,n=arr.size();
+        while(1){
+            mid=lo+(hi-lo)/2;
+            int i=0,j=n-2,c=0,N=0,D=1;
+            // cout<<mid<<" ";
+            while(i<n-1&&j>=0){
+                while(j>=0&&arr[i]>mid*arr[n-1-j]){
+                    j--;
+                }
+                if(j>=0){
+                    if(N*arr[n-1-j]<D*arr[i]){
+                        N=arr[i];
+                        D=arr[n-1-j];
+                    }
+                    c+=j+1;
+                    i++;
+                }   
+                
+            }
+            // cout<<c<<endl;
+            if(c==k){
+                return {N,D};
+            }
+            else if(c>k){
+                hi=mid;
+            }
+            else{
+                lo=mid;
+            }
         }
-        ans={arr[q.top().i],arr[q.top().j]};
-        return ans;
+        return {1,0};
     }
 };
