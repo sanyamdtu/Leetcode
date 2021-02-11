8f1886396f8fc2ff62f3e47b586f1599fe665f3c
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-02-11 13:14:57 +0530

    24 ms

--- solutions/1283. Find the Smallest Divisor Given a Threshold.cpp
+++ solutions/1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int smallestDivisor(vector<int>& nums, int h) {
-        int n=nums.size();
-        int beg=1,last=10000000,ans=10000000;
-        while(beg<=last){
-            int mid=(beg+last)/2;
-            cout<<mid<<" ";
-            long long sum=0;
-            for(int i=0;i<n;i++){
-                sum+=(nums[i]+mid-1)/mid;
-            }
-            if(sum<=h){
+    int smallestDivisor(vector<int>& arr, int h) {
+        int hi=1,lo=1,n=arr.size(),ans=INT_MAX;
+        for(auto i:arr)
+            hi=max(hi,i);
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int d=0;
+            for(auto i:arr)
+                d+=(i+mid-1)/mid;
+            if(d<=h)
+            {
+                hi=mid-1;
                 ans=min(ans,mid);
-                last=mid-1;
             }
             else
-                beg=mid+1;
+                lo=mid+1;
         }
         return ans;
+        
     }
 };
