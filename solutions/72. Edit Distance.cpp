a7f50ca7aa1a04df7902ca47ffaf517391fa372e
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-03-11 00:14:33 +0530

    4 ms

--- solutions/72. Edit Distance.cpp
+++ solutions/72. Edit Distance.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int minDistance(string a, string b) {
-        int n=a.length(),m=b.length();
-        if(m==0&&n==0)
-            return 0;
-        if(m==0)
-            return n;
+    int minDistance(string s, string t) {
+        int n=s.length(),m=t.length();
         if(n==0)
             return m;
+        if(m==0)
+            return n;
         int dp[n+1][m+1];
-        dp[0][0]=0;
-        for(int i=1;i<=n;i++)
-            dp[i][0]=1+dp[i-1][0];
-        for(int i=1;i<=m;i++)
-            dp[0][i]=1+dp[0][i-1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(a[i-1]==b[j-1])
-                    dp[i][j]=dp[i-1][j-1];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<n+1;i++)
+            dp[i][m]=n-i;
+        for(int i=0;i<m+1;i++)
+            dp[n][i]=m-i;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s[i]==t[j])
+                    dp[i][j]=dp[i+1][j+1];
                 else
-                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                    dp[i][j]=1+min(dp[i+1][j+1],min(dp[i+1][j],dp[i][j+1]));
             }
         }
-        return dp[n][m];
+        return dp[0][0];
+            
     }
 };
