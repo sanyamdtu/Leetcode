d4b1b1a7c4a5be7c36e5229fc1a113818f46a1a8
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-03-11 15:35:41 +0530

    12 ms

--- solutions/647. Palindromic Substrings.cpp
+++ solutions/647. Palindromic Substrings.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int ans=0,n=s.length();
-       for(int i=0;i<n;i++){
-           ans++;
-           int j=i-1,k=i+1;
-           while(k<n&&j>=0){
-               if(s[k]==s[j])
-                   ans++;
-               else
-                   break;
-               k++;
-               j--;
-           }
-           j=i,k=i+1;
-           while(k<n&&j>=0){
-               if(s[k]==s[j])
-                   ans++;
-               else
-                   break;
-               k++;
-               j--;
-           }
-           // j=i-1,k=i;
-           // while(k<n&&j>=0){
-           //     if(s[k]==s[j])
-           //         ans++;
-           //     else
-           //         break;
-           //     k++;
-           //     j--;
-           // }
-       }
+        int n=s.length();
+        if(n==0)
+            return 0;
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<n;i++)
+            dp[i][i]=1;
+        for(int gap=1;gap<n;gap++){
+            for(int l=0;l<n-gap;l++){
+                int r=l+gap;
+                if(s[l]==s[r]){
+                    if(l+1!=r)
+                        dp[l][r]=dp[l+1][r-1];
+                    else
+                        dp[l][r]=1;
+                }
+                    
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(dp[i][j]==1)
+                    ans++;
         return ans;
     }
 };
