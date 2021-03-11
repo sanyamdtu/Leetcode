400421a90d7b72ed3a3b576a12b9f5f719c368ff
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-03-11 15:18:03 +0530

    0 ms

--- solutions/413. Arithmetic Slices.cpp
+++ solutions/413. Arithmetic Slices.cpp
@@ -1,36 +1,16 @@
-int dp[5004][5005];
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& arr) {
-        int n=arr.size();
-        if(n<3)
-            return 0;
-        cout<<n;
-        
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n&&j<i+2;j++){
-                dp[i][j]=1;
+    int numberOfArithmeticSlices(vector<int>& a) {
+        int n=a.size();
+        int dp=0,ans=0;
+        for(int i=2;i<n;i++){
+            if(a[i]-a[i-1]==a[i-1]-a[i-2]){
+                dp++;
+                ans+=dp;
             }
+            else
+                dp=0;
         }
-       for(int i=0;i<n;i++){
-           for(int j=i+2;j<n;j++){
-               if(arr[j]-arr[j-1]==arr[j-1]-arr[j-2]){
-                   dp[i][j]=dp[i][j-1];
-                   // cout<<i<<" "<<j<<endl;
-               }
-               else
-                   dp[i][j]=0;
-           }
-       }
-        int ans=0;
-       for(int i=0;i<n;i++){
-           for(int j=i+2;j<n;j++){
-               ans+=dp[i][j];
-               
-           }
-       }
-       return ans;
-        return 0;
-       
+        return ans;
     }
 };
