e080ef879112800f09926749b753a3cea1c533f1
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-03-26 15:12:38 +0530

    4 ms

--- solutions/10. Regular Expression Matching.cpp
+++ solutions/10. Regular Expression Matching.cpp
@@ -1,3 +1,10 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.length(),m=p.length();
+        if(s==p)
+            return true;
+        int dp[n+1][m+1];
         memset(dp,0,sizeof(dp));
         dp[0][0]=1;
         for (int i = 2; i <=m; i++) 
@@ -7,7 +14,7 @@
             for(int j=1;j<=m;j++){
                 if(p[j-1]=='.'){
                     dp[i][j]=dp[i-1][j-1];
-                }
+                }
                 else if(p[j-1]=='*'){
                     if(dp[i][j-2]+dp[i][j-1]>0)
                         dp[i][j]=1;
