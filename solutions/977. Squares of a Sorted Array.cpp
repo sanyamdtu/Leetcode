0345d173473459302f43db412b06ee32550fa6bd
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-03-28 19:38:36 +0530

    28 ms

--- solutions/977. Squares of a Sorted Array.cpp
+++ solutions/977. Squares of a Sorted Array.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& arr) {
-        vector<int> ans;
-        int n=arr.size();
-        if(n==0)
-            return ans;
-        if(n==1){
-            ans.push_back(arr[0]*arr[0]);
-            return ans;
-​
-        }
-        int i=0;
-        while(i<n&&arr[i]<0)
-            i++;
-        int j=i-1;
-        while(i<n&&j>=0){
-            int x=arr[i]*arr[i];
-            int y=arr[j]*arr[j];
-            if(x>y){
-                ans.push_back(y);
+        vector<int> v;
+        for(int i=0,j=arr.size()-1;i<=j;){
+            long long a=arr[i]*arr[i];
+            long long b=arr[j]*arr[j];
+            v.push_back(max(a,b));
+            if(a<b)
                 j--;
-            }
-            else{
-                ans.push_back(x);
+            else
                 i++;
-            }
         }
-        while(i<n){
-            ans.push_back(arr[i]*arr[i]);
-            i++;
-        }
-        while(j>=0){
-            ans.push_back(arr[j]*arr[j]);
-            j--;
-        }
-        return ans;
-            
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
