dceb8d480ca44cfb1ecaeb7e5665b45300665b3c
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-04-07 16:01:10 +0530

    8 ms

--- solutions/345. Reverse Vowels of a String.cpp
+++ solutions/345. Reverse Vowels of a String.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int n=s.length();
-        int i=0,j=n-1;
-        while(i<j)
-        {
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'){
-                    swap(s[i],s[j]);
-                    i++;
-                    j--;
-                }
-                else
-                    j--;
-            }
-            else
+        for(int i=0,j=n-1;i<j;){
+            if(s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u'&&s[i]!='A'&&s[i]!='E'&&s[i]!='I'&&s[i]!='O'&&s[i]!='U')
                 i++;
-        }
+            else if(s[j]!='a'&&s[j]!='e'&&s[j]!='i'&&s[j]!='o'&&s[j]!='u'&&s[j]!='A'&&s[j]!='E'&&s[j]!='I'&&s[j]!='O'&&s[j]!='U')
+                j--;
+            else{
+                swap(s[i++],s[j--]);
+                
+            }
+                
+    }
         return s;
     }
 };
