5807960ec6ea82352e5aa97e89676f58143ed3b0
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-04-07 16:11:01 +0530

    4 ms

--- solutions/41. First Missing Positive.cpp
+++ solutions/41. First Missing Positive.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& arr) {
         int n=arr.size();
-        if(n==0)
-            return 1;
-        arr.push_back(arr[0]);
         for(int i=1;i<=n;){
-            if(arr[i]!=i){
-                if(arr[i]<1||arr[i]>n)
-                    i++;
-                else if(arr[i]!=arr[arr[i]])
-                    swap(arr[i],arr[arr[i]]);
-                else 
-                    i++;
+            int idx=i-1;
+            if(arr[idx]>0&&arr[idx]<=n&&arr[idx]!=arr[arr[idx]-1]){
+                swap(arr[idx],arr[arr[idx]-1]);
             }
             else
                 i++;
         }
-        for(int i=1;i<=n;i++)
-            if(arr[i]!=i)
+        for(int i=1;i<=n;i++){
+            if(arr[i-1]!=i)
                 return i;
+        }
         return n+1;
+        
     }
 };
