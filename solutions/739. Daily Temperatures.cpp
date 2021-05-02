9442c3f4b49237bfbc90db512aea308303c64653
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-05-02 16:11:16 +0530

    64 ms

--- solutions/739. Daily Temperatures.cpp
+++ solutions/739. Daily Temperatures.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& arr) {
         int n=arr.size();
-        vector<int>v(n);
-        if(n==0)
-            return v;
-        stack<pair<int,int>> s; 
-        s.push({arr[0],0});
-        for(int i=1;i<n;i++){
-            if(!s.empty()){
-                auto a=s.top();
-                while(!s.empty()&&arr[i]>a.first){
-                    v[a.second]=i-a.second;
-                    s.pop();
-                    if(!s.empty())
-                        a=s.top();
-                }
-            }
-            s.push({arr[i],i});
-        }
-        while(!s.empty()){
-            auto a=s.top();
+        int i=0;
+        stack<int> s;
+        vector<int> ans(n,0);
+        while(i<n){
+            while(!s.empty()&&arr[i]>arr[s.top()]){
+                ans[s.top()]=i-s.top();
                 s.pop();
-            v[a.second]=0;
+            }
+            s.push(i);
+            i++;
         }
-        return v;
-    }
+        return ans;
+    }
 };
