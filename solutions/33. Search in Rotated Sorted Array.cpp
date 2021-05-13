c6efb655b9b5f948912eadd3f895e5cb3f6e26d6
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-05-13 16:15:42 +0530

    4 ms

--- solutions/33. Search in Rotated Sorted Array.cpp
+++ solutions/33. Search in Rotated Sorted Array.cpp
@@ -2,23 +2,33 @@ class Solution {
 public:
     int search(vector<int>& arr, int k) {
         int n=arr.size();
-        int lo=0,hi=n-1;
+        int lo=0,hi=n-1,ans=-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(arr[mid]==k)
-                return mid;
-            if(arr[lo]<=arr[mid]){
-                if(arr[lo]<=k&&k<=arr[mid])
-                    hi=mid-1;
-                else
-                    lo=mid+1;
+            if(arr[mid]<arr[0])
+                hi=mid-1;
+            else 
+            {
+                ans=max(ans,mid);
+                lo=mid+1;
             }
-            else{
-                if(arr[mid]<=k&&k<=arr[hi])
-                    lo=mid+1;
-                else
-                    hi=mid-1;
-            }
+        } 
+        if(k<arr[0]){
+            lo=ans+1;
+            hi=n-1;
+        }
+        else{
+            lo=0;
+            hi=ans;
+        }
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]>k)
+                hi=mid-1;
+            else if(arr[mid]<k)
+                lo=mid+1;
+            else
+                return mid;
         }
         return -1;
     }
