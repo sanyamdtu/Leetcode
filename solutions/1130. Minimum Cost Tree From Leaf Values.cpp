f7df3df0c861ea85ace91d74e5a0fed8c5e4e342
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2021-06-19 12:37:48 +0530

    4 ms

--- solutions/1130. Minimum Cost Tree From Leaf Values.cpp
+++ solutions/1130. Minimum Cost Tree From Leaf Values.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        
+        stack<int> s;
+        int res=0;
+        for(auto i:arr){
+            while(!s.empty()&&s.top()<=i){
+                int a=s.top();
+                s.pop();
+                int c=INT_MAX;
+                if(!s.empty())
+                    c=s.top();
+                res+=a*min(c,i);
+            }
+            s.push(i);
+        }
+        while(s.size()>1){
+            int a=s.top();
+            s.pop();
+            res+=a*s.top();
+        }
+        return res;
     }
 };
