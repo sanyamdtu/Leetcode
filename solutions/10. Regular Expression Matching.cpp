a2ab95af52561cb565dd689e954d6f5d17987320
sanyamdtu <56213428+sanyamdtu@users.noreply.github.com>
2022-04-18 14:03:55 +0530

    3 ms

--- solutions/10. Regular Expression Matching.cpp
+++ solutions/10. Regular Expression Matching.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n=s.length(),m=p.length();
-        if(s==p)
-            return true;
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-        dp[0][0]=1;
-        for (int i = 2; i <=m; i++) 
-            if (p[i-1] == '*' && (dp[0][i-2]==1||dp[0][i-1]))
-               dp[0][i] = 1;
+        int n=s.length();
+        int m=p.length();
+        bool dp[200][200];  //   n and m
+        memset(dp,false,sizeof(dp));
+        dp[0][0]=true;
+        dp[0][1]=(p[0]=='*'||(m>=2&&p[1]=='*'));
+        for(int i=2;i<=m;i++){
+            if(p[i-1]=='*'){
+                dp[0][i]=dp[0][i-2];
+            }
+            else
+                dp[0][i]=0;
+        }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(p[j-1]=='.'){
+                if(s[i-1]==p[j-1]||p[j-1]=='.'){
                     dp[i][j]=dp[i-1][j-1];
-                }
-                else if(p[j-1]=='*'){
-                    if(dp[i][j-2]+dp[i][j-1]>0)
-                        dp[i][j]=1;
-                    else if(p[j-2]==s[i-1]||p[j-2]=='.')
-                        dp[i][j]=dp[i-1][j];
                 }
                 else{
-                    if(p[j-1]==s[i-1]){
-                        dp[i][j]=dp[i-1][j-1];
+                    if(p[j-1]=='*'){
+                       dp[i][j]=dp[i][j-2];
+                       dp[i][j]=(dp[i][j] || dp[i][j-1]); 
+                        if(s[i-1]==p[j-2] || p[j-2]=='.' )
+                        {
+                            dp[i][j]=(dp[i][j] || dp[i-1][j]);
+                        }
                     }
                 }
             }
         }
-       
-        return (dp[n][m]==1);
+        return dp[n][m];
     }
 };
